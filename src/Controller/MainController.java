a0848913bd5b8e39c0fcc70e5c3fcb320addd00e
YonaMoreda <yokots3q@gmail.com>
2020-08-13 23:51:50 +0200

    added controller/view start

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,4 +1,32 @@
 package Controller;
 
+import Model.Snake;
+import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 public class MainController {
+
+    @FXML
+    private GridPane center_grid;
+    private Snake snakeModel;
+
+    @FXML
+    public void initialize() {
+        snakeModel = new Snake();
+        paintSnake();
+    }
+
+    private void paintSnake() {
+        for (Point2D bodyPart : snakeModel.getBody()) {
+            Rectangle rectangle = new Rectangle(20, 20, Color.GREEN);
+            int col = (int) (bodyPart.getX() * 14);
+            int row = (int) (bodyPart.getY() * 14);
+            System.out.println(col + " " + row);
+            center_grid.add(rectangle, col, row); //c, r
+        }
+
+    }
 }
--- src/Model/Snake.java
+++ src/Model/Snake.java
@@ -2,17 +2,19 @@ package Model;
 
 import javafx.geometry.Point2D;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Snake {
 
-    private Point2D headPosition;
     private Direction direction;
     private int length;
-    private Queue<Point2D> body;
+
+    private final Queue<Point2D> body;
 
     public Snake() {
-        headPosition = new Point2D(0.5, 0.5);
+        body = new LinkedList<>();
+        body.add(new Point2D(0.5, 0.5));
         direction = Direction.RIGHT;
         length = 1;
     }
@@ -33,11 +35,16 @@ public class Snake {
     public boolean move() {
         Point2D delta = delta();
         for (Point2D bodyPart : body) {
-            bodyPart.add(delta.getX() / 7, delta.getY() / 7); // generalize this 7
+            bodyPart.add(delta.getX() / 7, delta.getY() / 7); // todo:: generalize this 7
             if (bodyPart.getX() < -1 || bodyPart.getX() > 1 || bodyPart.getY() < -1 || bodyPart.getY() > 1) {
                 return false;
             }
         }
         return true;
     }
+
+    public Queue<Point2D> getBody() {
+        return body;
+    }
+
 }
